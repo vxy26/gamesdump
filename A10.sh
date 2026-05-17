0ee0ac52590ed898d5c6600402f7bd394397a49e
vxy26 <inckamid@gmail.com>
2026-05-17 19:35:05 +0700

    Update A10.sh

--- A10.sh
+++ A10.sh
@@ -10,14 +10,17 @@ unzip -q -o "$BASE/afk.zip" -d "$BASE"
 su -c "
 mkdir -p "/storage/emulated/0/Android/data/dkapp.pol.seiyw/files"
 mkdir -p "/storage/emulated/0/Android/data/dkapp.pol.seiyx/files"
+mkdir -p "/storage/emulated/0/Android/data/dkapp.pol.seiyy/files"
 
 cp -r "$BASE/files/"* "/storage/emulated/0/Android/data/dkapp.pol.seiyw/files/"
 cp -r "$BASE/files/"* "/storage/emulated/0/Android/data/dkapp.pol.seiyx/files/"
+cp -r "$BASE/files/"* "/storage/emulated/0/Android/data/dkapp.pol.seiyy/files/"
 
 pm install -r \"$BASE/RotationControl.apk\" > /dev/null 2>&1
 pm install -r \"$BASE/1111fixlogin.apk\" > /dev/null 2>&1
 pm install -r \"$BASE/dl1.apk\" > /dev/null 2>&1
 pm install -r \"$BASE/dl2.apk\" > /dev/null 2>&1
+pm install -r \"$BASE/dl3.apk\" > /dev/null 2>&1
 
 settings put global window_animation_scale 0
 settings put global transition_animation_scale 0
